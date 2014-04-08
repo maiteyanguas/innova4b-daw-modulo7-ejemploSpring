c4d4db681717751dffc34f958226171838d25078
Maite <maite.yanguas@gmail.com>
2014-04-08 14:38:48 +0200

    Configuración de Spring

---
+++ src/test/java/innova4b/ejemploSpring/PeliculaFinderFromDBSpringTest.java
@@ -0,0 +1,27 @@
+package innova4b.ejemploSpring;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PeliculaFinderFromDBSpringTest {
+
+	PeliculaFinderFromDB peliculaFinderFromDB;
+	
+	@Before
+	public void setUp() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("innova4b/ejemploSpring/application-context.xml");
+		peliculaFinderFromDB = context.getBean("peliculaFinderFromDB",PeliculaFinderFromDB.class);
+	}
+
+	@Test
+	public void findAll_recupera_todas_las_peliculas_de_BD() {
+		assertEquals(5, peliculaFinderFromDB.findAll().size());
+	}
+
+}
---
+++ src/test/java/innova4b/ejemploSpring/PeliculaFinderFromTextSpringTest.java
@@ -0,0 +1,25 @@
+package innova4b.ejemploSpring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PeliculaFinderFromTextSpringTest {
+
+	PeliculaFinderFromText peliculaFinder;
+	
+	@Before
+	public void setUp() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("innova4b/ejemploSpring/application-context.xml");
+		peliculaFinder = context.getBean("peliculaFinderFromText",PeliculaFinderFromText.class);
+	}
+
+	@Test
+	public void findAll_recupera_la_lista_de_peliculas() {
+		assertEquals(6, peliculaFinder.findAll().size());
+	}
+
+}
